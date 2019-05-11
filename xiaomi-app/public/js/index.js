39c6e4c1be11755e13e3a939f7123dd6c1dc67cc
dengxuan102 <d474764333@163.com>
2019-05-11 15:17:57 +0800

    ""

---
+++ xiaomi-app/public/js/index.js
@@ -0,0 +1,15 @@
+(function() {
+  var topbar_cart = document.querySelector(".site-topbar>div>.topbar-cart");
+  var topbar_info = document.querySelector(".topbar-cart>.cart-menu");
+  console.log(topbar_cart);
+  // console.log(topbar_info);
+
+  topbar_cart.onmouseover = function() {
+    var topbar_cart = this;
+    topbar_info.style.display = "block";
+  };
+  topbar_cart.onmouseout = function() {
+    var topbar_cart = this;
+    topbar_info.style.display = "none";
+  };
+})();
