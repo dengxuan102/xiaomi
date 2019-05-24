61fbdb1497f71319b2f9232c1bbfa42638c2bfe5
dengxuan102 <d474764333@163.com>
2019-05-24 20:45:04 +0800

    ;;

--- public/js/header.js
+++ public/js/header.js
@@ -1,16 +1,5 @@
 $(() => {
-  $("#header-logo").mouseenter(function () {
-        
-  });
-
-
-
-
-
-
-
-
-  $(".site-header .search-text").focus(function() {
+  $(".site-header .search-text").focus(function () {
     $(this)
       .css("border", "1px solid #ff6700")
       .next()
@@ -19,7 +8,7 @@ $(() => {
       .addClass("hide");
     $(".site-header .search-form .keyword-list").removeClass("hide");
   });
-  $(".site-header .search-text").blur(function() {
+  $(".site-header .search-text").blur(function () {
     $(this)
       .css("border", "1px solid #e0e0e0")
       .next()
--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,36 @@
-$(document).ready(function() {
-  var mySwiper = new Swiper(".swiper-container", {
-    direction: "horizontal", // 垂直切换选项
-    loop: true, // 循环模式选项
-    effect: "fade",
-    // autoplay: true,
-    autoplay: {
-      delay: 2000,
-      stopOnLastSlide: false,
-      disableOnInteraction: true //用户操作swiper之后是否禁止autoplay，包括触碰，拖动，点击pagination等操作true/false
-    },
+$(() => {
+  $(document).ready(function () {
+    var mySwiper = new Swiper(".swiper-container", {
+      direction: "horizontal", // 垂直切换选项
+      loop: true, // 循环模式选项
+      effect: "fade",
+      // autoplay: true,
+      autoplay: {
+        delay: 2000,
+        stopOnLastSlide: false,
+        disableOnInteraction: true //用户操作swiper之后是否禁止autoplay，包括触碰，拖动，点击pagination等操作true/false
+      },
 
-    // 如果需要分页器
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: "true"
-    },
+      // 如果需要分页器
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: "true"
+      },
 
-    // 如果需要前进后退按钮
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    uniqueNavElements: false
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      uniqueNavElements: false
+    });
   });
 
 
-  
+  $(".home-main .home-brick-box .box-bd div li").mouseenter(function () {
+    $(this).addClass('brick-item-active');
+  });
+  $(".home-main .home-brick-box .box-bd div li").mouseleave(function () {
+    $(this).removeClass('brick-item-active');
+  })
 });
